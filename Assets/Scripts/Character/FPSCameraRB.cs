389c4a380b2502fcaa0031245b6bb6c6f8367379
Devon Oberdan <devonoberdan@gmail.com>
2025-05-20 10:32:46 -0700

    Brought in packages, dependencies like DOTween, and FPS code over from other project

---
+++ Assets/Scripts/Character/FPSCameraRB.cs
@@ -0,0 +1,80 @@
+﻿/* SETUP:
+ * 1. Set tag of camera to "MainCamera"
+ * 2. Make sure camera is not attached to player object
+ * 3. Attach script to camera
+ * 4. See FirstPersonMovementRB script to set up player object if needed
+ */
+
+using UnityEngine;
+
+public class FPSCameraRB : MonoBehaviour
+{
+    [SerializeField] private float lookVerticalSensitivity = 100f;
+    [SerializeField] private float lookHorizontalSensitivity = 100f;
+
+    Transform player;
+    CapsuleCollider playerCollider;
+
+    Vector3 cameraRotationInputVect;
+    Vector3 velocity;
+
+    float minAngle;
+    float maxAngle;
+    float rotationLerpSpeed;
+    float positionLerpSpeed;
+    float cameraHeightOffset;
+
+
+    float verticalRotation;
+
+    Vector3 playerTop;
+
+    public enum PositionStyle { Raw, Lerp }
+    public PositionStyle positionBehaviour = new PositionStyle();
+
+    public enum RotationStyle { Raw, Lerp }
+    public RotationStyle rotationBehaviour = new RotationStyle();
+
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerCollider = player.GetComponent<CapsuleCollider>();
+    }
+
+    void Start()
+    {       
+        cameraRotationInputVect = Vector3.zero;
+
+        minAngle = -90.0f;
+        maxAngle = 90.0f;
+        rotationLerpSpeed = 25.0f;
+        //positionLerpSpeed = 3.0f;
+        cameraHeightOffset = 0.1f;
+
+        playerTop = (player.transform.localPosition + Vector3.up * (playerCollider.height / 2));
+    }
+
+    void Update()
+    {
+        CameraRotation();
+    }
+
+    private void FixedUpdate()
+    {
+        player.transform.Rotate(0, Input.GetAxis("Mouse X") * lookHorizontalSensitivity/100f, 0);
+    }
+
+    void CameraRotation()
+    {
+        verticalRotation += Input.GetAxisRaw("Mouse Y") * lookVerticalSensitivity * Time.deltaTime;
+        verticalRotation = Mathf.Clamp(verticalRotation, minAngle, maxAngle);
+
+
+        if (rotationBehaviour == RotationStyle.Raw)
+            transform.localEulerAngles = Vector3.left * verticalRotation;
+        else
+            transform.localRotation = Quaternion.Lerp(gameObject.transform.localRotation, Quaternion.Euler(Vector3.left*verticalRotation), rotationLerpSpeed * Time.deltaTime);
+    }
+    
+}
