e98965f108f2745c965e63864d035c2b96ce2350
Devon Oberdan <devonoberdan@gmail.com>
2025-06-05 00:30:12 -0700

    Refactor of rotation handling/transitions in Detection script, resulting in smoother transition back into default rotation path

--- Assets/_Prototypes/BrutalistCity/Scripts/Detection.cs
+++ Assets/_Prototypes/BrutalistCity/Scripts/Detection.cs
@@ -1,4 +1,3 @@
-using DG.Tweening;
 using FinishOne.GeneralUtilities;
 using UnityEngine;
 
@@ -12,16 +11,20 @@ public class Detection : MonoBehaviour
     [SerializeField] private float detectAngle = 45f;
 
     [SerializeField] private float lockOnSpeed = 30f;
-    [SerializeField] private float timeToReturn = 1f;
+    [SerializeField] private float returnSpeed = 1.5f;
 
     [SerializeField] private InteractionBuffer detectionBuffer;
 
+    private Quaternion startRotation;
     private RaycastHit[] playerHit;
     private InteractionBuffer chargeBuffer;
     private RotateObject rotateObj;
-    private Vector3 rotationAtDetection;
+
+    private PathPoint returnPoint;
+    private float accumulatedAngle;
     private bool currentlyVisible;
-    private float dot;
+    private bool returning;
+
     private Vector3 TargetDir => target.position - transform.position;
 
     private bool Detected 
@@ -36,6 +39,8 @@ public class Detection : MonoBehaviour
         chargeBuffer = GetComponent<InteractionBuffer>();
         rotateObj = GetComponent<RotateObject>();
 
+        startRotation = transform.rotation;
+
         detectionBuffer.CooldownAndReset = false;
         detectionBuffer.OnComplete.AddListener(BeginLockOn);
     }
@@ -44,7 +49,6 @@ public class Detection : MonoBehaviour
     {
         if (!Detected)
         {
-            rotationAtDetection = transform.eulerAngles;
             Detected = true;
         }
     }
@@ -57,6 +61,7 @@ public class Detection : MonoBehaviour
         if(!Detected)
         {
             detectionBuffer.Interacting = currentlyVisible;
+            accumulatedAngle += rotateObj.RotationSpeed * rotateObj.DampenFactor * Time.deltaTime;
         }
         else
         {
@@ -66,23 +71,41 @@ public class Detection : MonoBehaviour
                 chargeBuffer.Interacting = currentlyVisible;
             }
 
+            // was seen, but is now hidden and not at all charged up
             if (chargeBuffer.Percentage == 0)
             {
                 detectionBuffer.Interacting = currentlyVisible;
 
                 if (currentlyVisible)
+                {
                     detectionBuffer.Complete();
+                }
             }
 
             if (currentlyVisible)
             {
+                returning = false;
                 Quaternion newRot = Quaternion.LookRotation(TargetDir, Vector3.up);
                 transform.rotation = Quaternion.Slerp(transform.rotation, newRot, lockOnSpeed * Time.deltaTime);
             }
-
-            if (detectionBuffer.Percentage == 0)
+            else if(detectionBuffer.Percentage == 0)
             {
-                transform.DORotate(rotationAtDetection, timeToReturn, RotateMode.Fast).SetEase(Ease.Linear).OnComplete(ReturnToPatrol);
+                if (!returning)
+                {
+                    returnPoint = FindReturnRotation();
+                }
+
+                returning = true;
+
+                float dot = Quaternion.Dot(transform.rotation, returnPoint.Rotation);
+                transform.rotation = Quaternion.Slerp(transform.rotation, returnPoint.Rotation, returnSpeed * Mathf.Abs(dot) * Time.deltaTime);
+
+                if(Mathf.Abs(dot) >= 0.999999)
+                {
+                    transform.rotation = returnPoint.Rotation;
+                    accumulatedAngle = returnPoint.Angle;
+                    ReturnToPatrol();
+                }
             }
         }
     }
@@ -99,14 +122,46 @@ public class Detection : MonoBehaviour
 
     private bool InViewingAngle()
     {
-        dot = Vector3.Dot(TargetDir.normalized, transform.forward);
-        return dot > Mathf.Cos(detectAngle * Mathf.Deg2Rad);
+        return Vector3.Dot(TargetDir.normalized, transform.forward) > Mathf.Cos(detectAngle * Mathf.Deg2Rad);
     }
 
     private bool HasLineOfSight()
     {
-        Ray ray = new(transform.position, target.transform.position-transform.position);
-        return Physics.SphereCastNonAlloc(ray, 10f, playerHit, detectRange, targetLayer) > 0;
+        return Physics.SphereCastNonAlloc(new Ray(transform.position, TargetDir), 10f, playerHit, detectRange, targetLayer) > 0;
+    }
+
+    private struct PathPoint
+    {
+        public Quaternion Rotation;
+        public float Angle;
+
+        public PathPoint(Quaternion Rotation, float Angle)
+        {
+            this.Rotation = Rotation;
+            this.Angle = Angle;
+        }
+    }
+
+    private PathPoint FindReturnRotation()
+    {
+        Quaternion bestRotation = Quaternion.identity;
+        float bestAngle = accumulatedAngle;
+        float smallestDifference = float.MaxValue;
+
+        for (float testAngle = accumulatedAngle - 180f; testAngle <= accumulatedAngle + 180f; testAngle += 1f)
+        {
+            Quaternion testRotation = startRotation * Quaternion.AngleAxis(testAngle, rotateObj.Vector.normalized);
+            float angleDiff = Quaternion.Angle(transform.rotation, testRotation);
+
+            if (angleDiff < smallestDifference)
+            {
+                smallestDifference = angleDiff;
+                bestAngle = testAngle;
+                bestRotation = testRotation;
+            }
+        }
+
+        return new PathPoint(bestRotation, bestAngle);
     }
 
     private void OnDrawGizmosSelected()
