64045a9607987f9c56d78b88ed4e88bc1ec12af2
Devon Oberdan <devonoberdan@gmail.com>
2025-06-07 17:37:48 -0700

    Various cleanup/polish of Booster and Grapple mechanics

--- Assets/_Prototypes/BrutalistCity/Scripts/Grapple.cs
+++ Assets/_Prototypes/BrutalistCity/Scripts/Grapple.cs
@@ -1,4 +1,6 @@
+using FinishOne.GeneralUtilities;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Grapple : MonoBehaviour
 {
@@ -12,13 +14,24 @@ public class Grapple : MonoBehaviour
     [SerializeField] private float grappleDelayTime;
     [SerializeField] private float overshootYAxis;
 
+    [SerializeField] private bool clampMagnitude;
+
+    [DrawIf(nameof(clampMagnitude), true)]
+    [SerializeField] private float maxMagnitude = 50;
+
+
     [Header("Cooldown")]
     [SerializeField] private float grappleCd;
     [Header("Input")]
     [SerializeField] private KeyCode grappleKey = KeyCode.Mouse1;
 
+    [SerializeField] private UnityEvent OnFired;
+    [SerializeField] private UnityEvent OnConnected;
+    [SerializeField] private UnityEvent OnMissed;
+
     private FPSMovementRB playerController;
-    private Camera cam;
+    private Rigidbody rb;
+    private Transform cam;
     private Vector3 grapplePoint;
     private float grappleCdTimer;
     private bool grappling;
@@ -31,9 +44,17 @@ public class Grapple : MonoBehaviour
             return;
         }
 
-        cam = Camera.main;
+        cam = Camera.main.transform;
         playerController = GetComponentInParent<FPSMovementRB>();
         playerController.onCollision += StopGrapple;
+
+        rb = GetComponentInParent<Rigidbody>();
+
+        if (!clampMagnitude)
+        {
+            maxMagnitude = -1f;
+        }
+
     }
 
     private void Update()
@@ -62,18 +83,19 @@ public class Grapple : MonoBehaviour
         if (grappleCdTimer > 0)
             return;
 
+        OnFired.Invoke();
         grappling = true;
 
-        playerController.Freeze = true;
-
-        if (Physics.Raycast(cam.transform.position, cam.transform.transform.forward, out RaycastHit hit, maxGrappleDistance, grappleLayers))
+        if (Physics.Raycast(cam.position, cam.forward, out RaycastHit hit, maxGrappleDistance, grappleLayers))
         {
+            playerController.Freeze = true;
             grapplePoint = hit.point;
             Invoke(nameof(ExecuteGrapple), grappleDelayTime);
         }
         else
         {
-            grapplePoint = cam.transform.position + cam.transform.forward * maxGrappleDistance;
+            grapplePoint = cam.position + cam.forward * maxGrappleDistance;
+            OnMissed.Invoke();
             Invoke(nameof(StopGrapple), grappleDelayTime);
         }
 
@@ -84,13 +106,42 @@ public class Grapple : MonoBehaviour
     private void ExecuteGrapple()
     {
         playerController.Freeze = false;
-
-        float grapplePointRelativeYPos = grapplePoint.y - (transform.position.y - 1f);
+        OnConnected.Invoke();
+        float grapplePointRelativeYPos = grapplePoint.y - transform.position.y - 1f;
         float highestPointOnArc = grapplePointRelativeYPos + overshootYAxis;
 
-        playerController.JumpToPosition(grapplePoint, highestPointOnArc);
+        Vector3 velocity = CalculateJumpVelocity(transform.position, grapplePoint, highestPointOnArc);
+        playerController.DelaySetVelocity(velocity);
+
         Invoke(nameof(StopGrapple), 1f);
     }
+
+    private Vector3 CalculateJumpVelocity(Vector3 startPoint, Vector3 endPoint, float trajectoryHeight)
+    {
+        float gravityForce = Mathf.Abs(Physics.gravity.y);
+        float displacementY = endPoint.y - startPoint.y;
+        Vector3 displacementXZ = (endPoint - startPoint).NewY(0);
+
+        // will pull us down if we grapple below us
+        float dir = Mathf.Sign(trajectoryHeight);
+        Vector3 velocityY = dir * Mathf.Sqrt(Mathf.Abs(2 * gravityForce * trajectoryHeight)) * Vector3.up;
+
+        float heightGravCalc = Mathf.Sqrt(Mathf.Abs(2 * trajectoryHeight / gravityForce));
+        float displacementHeightCalc = Mathf.Sqrt(Mathf.Abs(2 * displacementY - trajectoryHeight)) / gravityForce;
+
+        Vector3 velocityXZ = displacementXZ / (heightGravCalc + displacementHeightCalc);
+
+        Vector3 finalVelocity = velocityXZ + velocityY;
+
+        if (maxMagnitude > 0f && finalVelocity.magnitude > maxMagnitude)
+        {
+            finalVelocity = finalVelocity.normalized * maxMagnitude;
+        }
+
+        return finalVelocity;
+    }
+
+
 
     public void StopGrapple()
     {
