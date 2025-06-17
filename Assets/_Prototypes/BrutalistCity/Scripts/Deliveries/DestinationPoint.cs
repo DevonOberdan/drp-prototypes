47cf2a6eeba92f6acec930296c8356cd6efb086e
Trevor Simonson <trevjosh2000@gmail.com>
2025-06-17 16:55:03 -0400

    made it so you can now trigger dialogue on specific delivery doors if so desired. Changed main title text

--- Assets/_Prototypes/BrutalistCity/Scripts/Deliveries/DestinationPoint.cs
+++ Assets/_Prototypes/BrutalistCity/Scripts/Deliveries/DestinationPoint.cs
@@ -1,9 +1,12 @@
+using FinishOne.GeneralUtilities;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DestinationPoint : MonoBehaviour
 {
     [SerializeField] private Transform waypointRoot;
-
+    [SerializeField] private UnityEvent OnReached;
+    private TriggerEvent trigger;
     public Transform WayPointRoot 
     {
         get 
@@ -12,6 +15,29 @@ public class DestinationPoint : MonoBehaviour
                 waypointRoot = transform;
 
             return waypointRoot;
+        }
+    }
+    public bool WaypointActive => waypointRoot.childCount > 0;
+    public void SetupWaypoint(GameObject prefab)
+    {
+        GameObject waypoint = Instantiate(prefab, WayPointRoot);
+
+        if (waypoint.TryGetComponent(out trigger))
+        {
+            trigger.TriggerEnterEvent.AddListener(() => OnReached.Invoke());
+        }
+    }
+
+    internal void ClearWaypoint()
+    {
+        if (trigger != null)
+        {
+            trigger.TriggerEnterEvent.RemoveListener(() => OnReached.Invoke());
+        }
+
+        for (int i = WayPointRoot.childCount - 1; i >= 0; i--)
+        {
+            Destroy(WayPointRoot.GetChild(i).gameObject);
         }
     }
 }
