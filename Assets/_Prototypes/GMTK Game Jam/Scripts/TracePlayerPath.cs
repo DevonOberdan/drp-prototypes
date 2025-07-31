ec9816eef6327678c195f66a16858334d44be4b1
Devon Oberdan <devonoberdan@gmail.com>
2025-07-30 18:51:42 -0700

    simple loop detection setup

--- Assets/_Prototypes/GMTK Game Jam/Scripts/TracePlayerPath.cs
+++ Assets/_Prototypes/GMTK Game Jam/Scripts/TracePlayerPath.cs
@@ -6,6 +6,7 @@ public class TracePlayerPath : MonoBehaviour
     [SerializeField] private LineRenderer lineRenderer;
 
     [SerializeField] private float maxDist = 0.1f;
+    [SerializeField] private int minimumPointGap = 15;
 
     private float step = 0.1f;
     private float time = 0;
@@ -21,6 +22,8 @@ public class TracePlayerPath : MonoBehaviour
         if (Vector3.Distance(transform.position, lineRenderer.GetPosition(lineRenderer.positionCount - 1)) > maxDist)
         {
             AddPoint();
+
+            DetectLoop();
         }
     }
 
@@ -28,5 +31,18 @@ public class TracePlayerPath : MonoBehaviour
     {
         lineRenderer.positionCount++;
         lineRenderer.SetPosition(lineRenderer.positionCount - 1, transform.position.NewY(.15f));
+    }
+
+    private void DetectLoop()
+    {
+        Vector3 newPoint = lineRenderer.GetPosition(lineRenderer.positionCount - 1);
+
+        for (int i = 0; i < lineRenderer.positionCount - minimumPointGap; i++)
+        {
+            if(Vector3.Distance(newPoint, lineRenderer.GetPosition(i)) < 0.15f)
+            {
+                Debug.Log("Closed loop!");
+            }
+        }
     }
 }
